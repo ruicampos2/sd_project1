06e1ff142f4a60b2085d6226222fba6399415f72
Miguel Ferreira <migueltf@ua.pt>
2024-03-14 05:41:49 +0000

    entity states

---
+++ Entities/TCoachStates.java
@@ -0,0 +1,10 @@
+package Entities;
+
+public class TCoachStates {
+
+    public static final int WAIT_FOR_REFEREE_COMMAND = 0;
+
+    public static final int ASSEMBLE_TEAM = 1;
+
+    public static final int WATCH_TRIAL = 2;
+}
---
+++ Entities/TContestantStates.java
@@ -0,0 +1,10 @@
+package Entities;
+
+public class TContestantStates {
+
+    public static final int SEAT_AT_BENCH = 0;
+
+    public static final int STAND_IN_POSITION = 1;
+
+    public static final int DO_YOUR_BEST = 2;
+}
