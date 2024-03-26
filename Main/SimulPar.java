bd6e80d3a2dc22a226a18cff9f2d1e63332b53ca
ruicampos2 <ruigabriel2@ua.pt>
2024-03-26 23:06:48 +0000

    upd

--- Main/SimulPar.java
+++ Main/SimulPar.java
@@ -1,17 +1,54 @@
 package Main;
 
-public final class SimulPar {
-
+/**
+ *    Definition of the simulation parameters.
+ */
+public final class SimulPar
+{
     /**
      * Number of teams.
      */
     public static final int NUMBER_OF_TEAMS = 2;
 
     /**
+     *   Number of maximum games in each match
+     */
+    public static final int GAMES_PER_MATCH = 3;
+
+    /**
+     *   Number of maximum trials in each game
+     */
+    public static final int TRIALS_PER_GAME = 6;
+
+    /**
+     *   Number of maximum players in the match.
+     */
+    public static final int MAX_CONTESTANTS = 10;
+    // public static final int MAX_TEAM_ELEMENTS = 5;
+
+    public static final int MAX_COACHES = 2;    
+
+    public static final int MIN_CONTESTANT_STRENGTH = 1;    
+    
+    public static final int MAX_CONTESTANT_STRENGTH = 5;    
+
+    /**
+     *   Number of maximum players playing the Trial
+     */
+    public static final int MAX_PLAYERS_PLAYING = 6;
+
+    /**
+     *   Number of maximum displacement between ordinary thieves.
+     */
+
+    /**
      * Number of players per team.
      */
     public static final int NUMBER_OF_PLAYERS = 5;
 
-
-
+        /**
+     *   It can not be instantiated.
+     */
+    private SimulPar ()
+    { }
 }
